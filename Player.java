f4db8f31b0618cbef95d527f1e58c1667fcdfaf6
Jewduh <150200669+Jewduh@users.noreply.github.com>
2025-01-19 12:47:12 -0500

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,8 @@
+public class Player {
+    public String player_choice;
+    public Boolean have_fancy_coin;
+    public String player_name;
+    public int playerScore;
+    public int playerHighScore;
+
+}
