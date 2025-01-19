ccb89c10fda67ed82cfc359455059a9c1739a14d
Jewduh <150200669+Jewduh@users.noreply.github.com>
2025-01-19 12:50:14 -0500

    Create DevSpec.java

---
+++ DevSpec.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class DevSpec {
+    public int maxDevHealth;
+    public int devHealth;
+    public int devAgility;
+    public int devDamage;
+    public int devCritChance;
+    public int devCritDamage;
+    public boolean devDead;
+
+    ArrayList<String> Divinity = new ArrayList<>();
+    ArrayList <String> inventory = new ArrayList<>();
+}
