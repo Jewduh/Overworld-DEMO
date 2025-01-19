703349dd1adf80142ff17c1526cfb33226794290
Jewduh <150200669+Jewduh@users.noreply.github.com>
2025-01-19 12:51:10 -0500

    Create Mechanics.java

---
+++ Mechanics.java
@@ -0,0 +1,8 @@
+public class Mechanics {
+    public int investmentPoint;
+    public int smallHealthPotion;
+    public int largeHealthPotion;
+    public int engravedKey;
+    public int randoCrit;
+    public int randoDodge;
+}
