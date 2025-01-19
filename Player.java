a8be2458fd9f6507c8eff15c9cc0c5300ff0ab33
Jewduh <150200669+Jewduh@users.noreply.github.com>
2025-01-19 12:48:45 -0500

    Delete Player.java

--- Player.java
+++
@@ -1,8 +0,0 @@
-public class Player {
-    public String player_choice;
-    public Boolean have_fancy_coin;
-    public String player_name;
-    public int playerScore;
-    public int playerHighScore;
-
-}
